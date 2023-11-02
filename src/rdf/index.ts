ac3965278735315ad8c8fe4cac5f2192a6e75bb2
Sebastian Faubel <sebastian.faubel@gmail.com>
2023-11-02 19:57:57 +0100

    Exporting IndividualRepository.

--- src/rdf/index.ts
+++ src/rdf/index.ts
@@ -1,5 +1,6 @@
 export * from "./class-repository";
 export * from "./property-repository";
+export * from "./individual-repository";
 export * from "./owl-reasoner";
 export * from "./rdfs-reasoner";
 export * from "./store-factory";
